1cc323d4b70808038189cc5b7e466d894fce7f78
David Todorov <david.todorr@gmail.com>
2023-11-05 13:42:29 +0100

    Api interfaces updated

--- backend/src/main/java/com/moodify/backend/domain/services/ApiService.java
+++ backend/src/main/java/com/moodify/backend/domain/services/ApiService.java
@@ -7,12 +7,12 @@ import java.util.List;
 public interface ApiService {
 
     TrackTO getTrack (long id);
+    AlbumTO getAlbum(long id);
+    PlaylistTO getPlaylist(long id);
+    ArtistTO getArtist(long id);
     List<TrackTO> getTrackSearch (String query);
-
     List<ArtistTO> getArtists(String query);
-
     List<AlbumTO> getAlbums(String query);
-
     List<PlaylistTO> getPlaylists(String query);
 
 }
